f001276a22fd254b1f38beb84938e843b17608f3
Stijn Voeten <stijn.voeten01@student.ap.be>
2025-02-24 14:34:59 +0100

    backend fixes

--- packages/common/src/entity/project.entity.ts
+++ packages/common/src/entity/project.entity.ts
@@ -4,5 +4,16 @@ export class Project {
   public updatedAt: Date;
   public title: string;
   public userId: number;
-  public public: number;
+  public public: boolean;
+
+  public static fromJson(json: any): Project {
+    const project = new Project();
+    project.id = json.id;
+    project.createdAt = new Date(json.createdAt);
+    project.updatedAt = new Date(json.updatedAt);
+    project.title = json.title;
+    project.userId = json.userId;
+    project.public = json.public;
+    return project;
+  }
 }
