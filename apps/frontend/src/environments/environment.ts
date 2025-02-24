e00d6ab5def67b57029c669a2048c560d38b2aff
Ghanoudi Hakim <hakim.ghanoudi@student.ap.be>
2025-02-24 09:46:39 +0100

    Add environment ts for frontend

---
+++ apps/frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  keycloakRealm: 'AP_Terra',
+  keycloakUrl: 'http://localhost:8080/auth',
+  keycloakClientId: `Frontend`,
+};
