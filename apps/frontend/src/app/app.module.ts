b224fb58776ad1aa6a339780f6a7d8950bfcd6eb
Ghanoudi Hakim <hakim.ghanoudi@student.ap.be>
2025-02-24 09:43:57 +0100

    Add some routing

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,25 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideKeycloak } from 'keycloak-angular';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [
-    provideKeycloak({
-      config: {
-        url: 'keycloak.iot-ap.be',
-        realm: 'apterra',
-        clientId: 'frontend',
-      },
-      initOptions: {
-        onLoad: 'login-required',
-        silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
-      },
-    }),
-  ],
+  imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
