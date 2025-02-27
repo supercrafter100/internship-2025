b46ea5870bf77c17103f8ea45749213426ec77ad
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2025-02-27 20:42:35 +0100

    attempt to rename realm of keycloak to functioning one

--- apps/frontend/src/environments/environment.ts
+++ apps/frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: '/api',
-  keycloakRealm: 'AP_Terra',
+  keycloakRealm: 'DEV_AP_Terra',
   keycloakUrl: 'https://keycloak.iot-ap.be',
   keycloakClientId: `Frontend`,
 };
