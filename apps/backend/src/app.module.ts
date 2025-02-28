31b2e24f7556843207ccdf2bf56875d1fb73c2cd
Ghanoudi Hakim <hakim.ghanoudi@student.ap.be>
2025-02-28 14:49:08 +0100

    Remove device module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,14 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { DevicesModule } from './devices/devices.module';
+
 import { ProjectController } from './controllers/project/project.controller';
 import { ProjectService } from './services/project/project.service';
 import { MinioClientModule } from './minio-client/minio-client.module';
 import { InfluxdbModule } from './influxdb/influxdb.module';
 
 @Module({
-  imports: [PrismaModule, DevicesModule, InfluxdbModule, MinioClientModule],
+  imports: [PrismaModule, InfluxdbModule, MinioClientModule],
   controllers: [AppController, ProjectController],
   providers: [AppService, PrismaService, ProjectService],
 })
