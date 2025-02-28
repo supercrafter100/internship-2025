dd7e3a695a932bcbcf3b44ec310f721385ebf00f
Stijn Voeten <stijn.voeten01@student.ap.be>
2025-02-28 14:21:48 +0100

    potentially fix docker compose

--- apps/backend/src/minio-client/config.ts
+++ apps/backend/src/minio-client/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   MINIO_ENDPOINT:
-    process.env.MINIO_ENDPOINT ?? 'https://minio-dev.bsaffer.iot-ap.be',
+    process.env.MINIO_ENDPOINT ?? 'minio-staging.bsaffer.iot-ap.be',
   MINIO_ACCESSKEY: process.env.MINIO_ACCESSKEY ?? 'bsaffer',
   MINIO_SECRETKEY: process.env.MINIO_SECRETKEY ?? 'adminadmin',
   MINIO_BUCKET: process.env.MINIO_BUCKET ?? 'bsaffer',
