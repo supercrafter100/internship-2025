e900cc4eaa2cf38ead67ecf25f42d8af19d4345c
Ghanoudi Hakim <hakim.ghanoudi@student.ap.be>
2025-02-28 14:02:58 +0100

    Add influxDB service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { DevicesModule } from './devices/devices.module';
 import { ProjectController } from './controllers/project/project.controller';
 import { ProjectService } from './services/project/project.service';
+import { InfluxdbModule } from './influxdb/influxdb.module';
 
 @Module({
-  imports: [PrismaModule, DevicesModule],
+  imports: [PrismaModule, DevicesModule, InfluxdbModule],
   controllers: [AppController, ProjectController],
   providers: [AppService, PrismaService, ProjectService],
 })
