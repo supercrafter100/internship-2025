f951c204df462254642daf79acb89a286cfaf17c
Stijn Voeten <stijn.voeten01@student.ap.be>
2025-03-10 13:49:48 +0100

    properly show background image for newly created projects

--- apps/frontend/src/util/utils.ts
+++ apps/frontend/src/util/utils.ts
@@ -1,3 +1,5 @@
+import { environment } from '../environments/environment';
+
 export function toBase64(file: File): Promise<string> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -5,4 +7,12 @@ export function toBase64(file: File): Promise<string> {
     reader.onload = () => resolve(reader.result as string);
     reader.onerror = (error) => reject(error);
   });
+}
+
+export function parseCDNUrl(url: string) {
+  if (url.startsWith('http')) {
+    return url;
+  }
+
+  return `${environment.cdnUrl}${url}`;
 }
