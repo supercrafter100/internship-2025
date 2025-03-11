ecb1682e9e5984276638f51c57e4f8346599c4d9
Stijn Voeten <stijn.voeten01@student.ap.be>
2025-03-11 12:23:21 +0100

    add dotenv

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { json } from 'express';
+import { config } from 'dotenv';
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
