074ccd8b37f6baf0aa99b46c99b1756aa5009515
Ghanoudi Hakim <hakim.ghanoudi@student.ap.be>
2025-03-11 08:07:22 +0100

    Add keycloak to Nest JS

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -11,9 +11,44 @@ import { InfluxdbModule } from './influxdb/influxdb.module';
 import { DevicesController } from './controllers/device/devices.controller';
 import { DeviceService } from './services/device/device.service';
 
+import { APP_GUARD } from '@nestjs/core'; //its opsional to global level authentication guard
+import {
+  AuthGuard,
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+} from 'nest-keycloak-connect';
+import { KeycloakConfigService } from './keycloak/config/keycloak-config.service';
+import { ConfigModules } from './keycloak/config/config.module';
+
 @Module({
-  imports: [PrismaModule, InfluxdbModule, MinioClientModule],
+  imports: [
+    PrismaModule,
+    InfluxdbModule,
+    MinioClientModule,
+    KeycloakConnectModule.registerAsync({
+      useExisting: KeycloakConfigService,
+      imports: [ConfigModules],
+    }),
+  ],
   controllers: [AppController, ProjectController, DevicesController],
-  providers: [AppService, PrismaService, ProjectService, DeviceService],
+  providers: [
+    AppService,
+    PrismaService,
+    ProjectService,
+    DeviceService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard, //By default, it will throw a 401 unauthorized when it is unable to verify the JWT token or Bearer header is missing.
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard, //Only controllers annotated with @Resource and methods with @Scopes are handled by this guard
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard, //Permissive by default. Used by controller methods annotated with @Roles
+    },
+  ],
 })
 export class AppModule {}
