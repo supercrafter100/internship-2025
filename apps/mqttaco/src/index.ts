d069b32670f3a2f0c178f4fa9e119fafc5fcb1b0
Stijn Voeten <stijn.voeten01@student.ap.be>
2025-04-25 00:24:26 +0200

    rewrite weird imports

--- apps/mqttaco/src/index.ts
+++ apps/mqttaco/src/index.ts
@@ -1,10 +1,10 @@
 // src/index.ts
-import { MqttService } from "./mqtt/mqttService";
-import { TtnClientManager } from "./ttn/ttn_clientManager"; // Zorg ervoor dat je manager importeert
-import { startHttpServer } from "../src/webserver";
+import { MqttService } from './mqtt/mqttService';
+import { TtnClientManager } from './ttn/ttn_clientManager'; // Zorg ervoor dat je manager importeert
+import { startHttpServer } from './webserver';
 
 const mqttService = new MqttService();
-console.log("🚀 MQTT Service gestart...");
+console.log('🚀 MQTT Service gestart...');
 
 // Maak de TtnClientManager aan om TTN-clients te beheren
 const ttnClientManager = new TtnClientManager();
@@ -14,12 +14,12 @@ startHttpServer(ttnClientManager);
 
 // Functie om de TTN-servers te synchroniseren
 async function syncTtnClients() {
-  console.log("🔄 Synchroniseren van TTN-clients vanuit de database...");
-  await ttnClientManager.syncFromDatabase(); // Dit haalt servers op en start clients
-  console.log("🌐 Alle TTN-clients zijn gesynchroniseerd.");
+    console.log('🔄 Synchroniseren van TTN-clients vanuit de database...');
+    await ttnClientManager.syncFromDatabase(); // Dit haalt servers op en start clients
+    console.log('🌐 Alle TTN-clients zijn gesynchroniseerd.');
 }
 
 // Start de synchronisatie van TTN-clients
 syncTtnClients().catch((err) => {
-  console.error("❌ Fout bij synchroniseren van TTN-clients:", err);
+    console.error('❌ Fout bij synchroniseren van TTN-clients:', err);
 });
