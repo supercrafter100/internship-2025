b6b2f5caa4aed99a9dc86ae50d0d291b60e30011
Stijn Voeten <stijn.voeten01@student.ap.be>
2025-04-25 08:53:18 +0200

    add proper migration

--- apps/backend/prisma/migrations/20250425065248_init/migration.sql
+++ apps/backend/prisma/migrations/20250425065248_init/migration.sql
@@ -1,20 +1,11 @@
 -- CreateEnum
 CREATE TYPE "DashboardType" AS ENUM ('STANDARD', 'CUSTOM');
 
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('ADMIN', 'MEMBER');
-
--- CreateEnum
-CREATE TYPE "DeviceType" AS ENUM ('WIMV1', 'ROBV1', 'GATE');
-
--- CreateEnum
-CREATE TYPE "Protocol" AS ENUM ('LTE', 'LORA', 'WIFI');
-
 -- CreateTable
 CREATE TABLE "Dashboard" (
     "id" SERIAL NOT NULL,
     "projectId" INTEGER NOT NULL,
-    "preset" JSONB NOT NULL,
+    "preset" JSONB,
     "type" "DashboardType" NOT NULL,
 
     CONSTRAINT "Dashboard_pkey" PRIMARY KEY ("id")
@@ -26,10 +17,25 @@ CREATE TABLE "Project" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "title" VARCHAR(255) NOT NULL,
+    "shortDescription" VARCHAR(255) NOT NULL,
     "userId" INTEGER NOT NULL,
     "public" BOOLEAN NOT NULL,
+    "imgKey" VARCHAR(255) NOT NULL,
 
     CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TtnProvider" (
+    "id" SERIAL NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    "appUrl" VARCHAR(255) NOT NULL,
+    "appId" VARCHAR(255) NOT NULL,
+    "apiKey" VARCHAR(500) NOT NULL,
+    "addedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdByid" INTEGER,
+
+    CONSTRAINT "TtnProvider_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -37,7 +43,7 @@ CREATE TABLE "ProjectUser" (
     "id" SERIAL NOT NULL,
     "projectId" INTEGER NOT NULL,
     "userId" INTEGER NOT NULL,
-    "role" "Role" NOT NULL,
+    "admin" BOOLEAN NOT NULL,
 
     CONSTRAINT "ProjectUser_pkey" PRIMARY KEY ("id")
 );
@@ -56,7 +62,7 @@ CREATE TABLE "Tile" (
     "id" SERIAL NOT NULL,
     "title" VARCHAR(255) NOT NULL,
     "description" VARCHAR(500) NOT NULL,
-    "img" VARCHAR(500) NOT NULL,
+    "imgKey" VARCHAR(255) NOT NULL,
     "launchpadId" INTEGER NOT NULL,
 
     CONSTRAINT "Tile_pkey" PRIMARY KEY ("id")
@@ -65,50 +71,105 @@ CREATE TABLE "Tile" (
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
+    "providerId" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "name" TEXT NOT NULL,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Device" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
+    "deviceType" TEXT NOT NULL,
     "projectId" INTEGER NOT NULL,
     "name" VARCHAR(255) NOT NULL,
-    "geoLocation" TEXT,
-    "image" TEXT,
+    "description" VARCHAR(500) NOT NULL,
+    "latitude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
+    "imgKey" VARCHAR(255) NOT NULL,
+    "createdByid" INTEGER,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "type" "DeviceType" NOT NULL,
-    "protocol" "Protocol" NOT NULL,
 
     CONSTRAINT "Device_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
+CREATE TABLE "deviceParameters" (
+    "id" SERIAL NOT NULL,
+    "deviceId" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(500) NOT NULL,
+
+    CONSTRAINT "deviceParameters_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TtnDeviceDetail" (
+    "id" SERIAL NOT NULL,
+    "deviceId" TEXT NOT NULL,
+    "ttnDeviceId" VARCHAR(255) NOT NULL,
+    "ttnProviderId" INTEGER NOT NULL,
+
+    CONSTRAINT "TtnDeviceDetail_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Video" (
     "id" SERIAL NOT NULL,
-    "deviceId" INTEGER NOT NULL,
+    "deviceId" TEXT NOT NULL,
     "videoUrl" VARCHAR(500) NOT NULL,
     "recordedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "ApiKey" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "projectId" INTEGER NOT NULL,
+
+    CONSTRAINT "ApiKey_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Dashboard_projectId_key" ON "Dashboard"("projectId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProjectUser_projectId_userId_key" ON "ProjectUser"("projectId", "userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Launchpad_projectId_key" ON "Launchpad"("projectId");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_providerId_key" ON "User"("providerId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Device_id_key" ON "Device"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TtnDeviceDetail_deviceId_key" ON "TtnDeviceDetail"("deviceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ApiKey_key_key" ON "ApiKey"("key");
 
 -- AddForeignKey
 ALTER TABLE "Dashboard" ADD CONSTRAINT "Dashboard_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TtnProvider" ADD CONSTRAINT "TtnProvider_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TtnProvider" ADD CONSTRAINT "TtnProvider_createdByid_fkey" FOREIGN KEY ("createdByid") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ProjectUser" ADD CONSTRAINT "ProjectUser_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -126,4 +187,19 @@ ALTER TABLE "Tile" ADD CONSTRAINT "Tile_launchpadId_fkey" FOREIGN KEY ("launchpa
 ALTER TABLE "Device" ADD CONSTRAINT "Device_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Device" ADD CONSTRAINT "Device_createdByid_fkey" FOREIGN KEY ("createdByid") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "deviceParameters" ADD CONSTRAINT "deviceParameters_deviceId_fkey" FOREIGN KEY ("deviceId") REFERENCES "Device"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TtnDeviceDetail" ADD CONSTRAINT "TtnDeviceDetail_deviceId_fkey" FOREIGN KEY ("deviceId") REFERENCES "Device"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TtnDeviceDetail" ADD CONSTRAINT "TtnDeviceDetail_ttnProviderId_fkey" FOREIGN KEY ("ttnProviderId") REFERENCES "TtnProvider"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Video" ADD CONSTRAINT "Video_deviceId_fkey" FOREIGN KEY ("deviceId") REFERENCES "Device"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApiKey" ADD CONSTRAINT "ApiKey_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
