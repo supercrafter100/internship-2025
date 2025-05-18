7ee0a50eafa85ad1a27a84bdd653f02609993b5f
Ghanoudi Hakim <hakim.ghanoudi@student.ap.be>
2025-05-18 16:46:57 +0300

    Add device create test for device service

--- apps/backend/src/services/device/device.service.spec.ts
+++ apps/backend/src/services/device/device.service.spec.ts
@@ -3,6 +3,10 @@ import { DeviceService } from './device.service';
 import { PrismaService } from '../../prisma/prisma.service';
 import { InfluxdbService } from '../../influxdb/influxdb.service';
 import { MinioClientService } from '../../minio-client/minio-client.service';
+import { Decimal } from '@prisma/client/runtime/library';
+import { CreateDeviceDto } from '@bsaffer/api/device/dto/create-device.dto';
+import { de } from '@faker-js/faker/.';
+import { mock } from 'node:test';
 
 describe('DeviceService', () => {
   let service: DeviceService;
@@ -51,5 +55,86 @@ describe('DeviceService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  //BACKEND TESTS 01_2
+  it('should create a device', async () => {
+    const mockDevice: CreateDeviceDto = {
+      projectId: 1,
+      deviceType: 'sensor',
+      deviceName: 'Test Device',
+      deviceImage: 'base64ImageString',
+      deviceDescription: 'Test Description',
+      latitude: 52.3702,
+      longitude: 4.8952,
+      deviceParameters: [{ name: 'temperature', description: 'bme280' }],
+    };
+
+    const expectedResult = {
+      id: 'abc123',
+      deviceType: 'sensor',
+      projectId: 1,
+      name: 'Test Device',
+      description: 'Test Description',
+      latitude: '52.3702' as unknown as Decimal,
+      longitude: '4.8952' as unknown as Decimal,
+      imgKey: 'base64ImageString',
+      createdByid: null,
+      createdAt: new Date(),
+      deviceParameters: [
+        {
+          id: 1,
+          deviceId: 'abc123',
+          name: 'temperature',
+          description: 'bme280',
+        },
+      ],
+    };
+
+    const expectedPrismaResult = {
+      id: expectedResult.id,
+      createdAt: expectedResult.createdAt,
+      createdByid: expectedResult.createdByid,
+      deviceType: mockDevice.deviceType.toString(),
+      imgKey: expectedResult.imgKey,
+      name: mockDevice.deviceName,
+      description: mockDevice.deviceDescription,
+      latitude: mockDevice.latitude.toString(),
+      longitude: mockDevice.longitude.toString(),
+      projectId: mockDevice.projectId,
+      deviceParameters: [
+        {
+          id: 1,
+          deviceId: 'abc123',
+          name: 'temperature',
+          description: 'bme280',
+        },
+      ],
+    };
+
+    const expectedPrismaCall = {
+      deviceType: mockDevice.deviceType.toString(),
+      imgKey: 'base64ImageString',
+      name: mockDevice.deviceName,
+      description: mockDevice.deviceDescription,
+      latitude: mockDevice.latitude.toString(),
+      longitude: mockDevice.longitude.toString(),
+      projectId: mockDevice.projectId,
+      deviceParameters: {
+        create: mockDevice.deviceParameters,
+      },
+    };
+
+    // Mocked created device
+    mockPrismaService.device.create.mockResolvedValue(expectedPrismaResult);
+    mockMinioClientService.uploadDeviceBase64Image.mockResolvedValue(
+      'base64ImageString',
+    );
+
+    const result = await service.create(mockDevice);
+    expect(result).toEqual(expectedResult);
+    expect(mockPrismaService.device.create).toHaveBeenCalledWith({
+      data: expectedPrismaCall,
+    });
   });
 });
