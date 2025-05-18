0c5dfb3aed07273f75f0c149ddce3ea3d44fdc8a
Ghanoudi Hakim <hakim.ghanoudi@student.ap.be>
2025-05-18 13:21:34 +0300

    Add project creation test for project service

--- apps/backend/src/services/project/project.service.spec.ts
+++ apps/backend/src/services/project/project.service.spec.ts
@@ -2,6 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectService } from './project.service';
 import { MinioClientService } from '../../minio-client/minio-client.service';
 import { PrismaService } from '../../prisma/prisma.service';
+import { title } from 'process';
+import { CreateProjectDto } from '@bsaffer/api/project/dto/create-project.dto';
+import { create } from 'domain';
+import { console } from 'inspector';
 
 describe('ProjectService', () => {
   let service: ProjectService;
@@ -32,5 +36,42 @@ describe('ProjectService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  //BACKEND TEST 01_1
+  //Unit test die de service waarmee een project wordt aangemaakt in de backend zal verifiëren
+  it('should create a project', async () => {
+    // Demo project
+    const demoProject: CreateProjectDto = {
+      title: 'Test Project',
+      shortDescription: 'Test Description',
+      base64Image: 'base64ImageString',
+      userId: 0,
+      public: false,
+    };
+
+    // Mocked created project
+    const expectedReturnProject = {
+      id: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      imgKey: 'base64Image',
+      ...demoProject,
+    };
+
+    const { base64Image, ...rest } = demoProject;
+    const expectedPrismaCall = { ...rest, imgKey: 'base64Image' };
+
+    mockPrismaService.project.create.mockResolvedValue(expectedReturnProject);
+    mockMinioClientService.uploadBase64Image.mockResolvedValue('base64Image');
+
+    const result = await service.create(demoProject);
+    expect(result).toEqual(expectedReturnProject);
+    expect(mockPrismaService.project.create).toHaveBeenCalledWith({
+      data: expectedPrismaCall,
+    });
+    expect(mockMinioClientService.uploadBase64Image).toHaveBeenCalledWith(
+      demoProject.base64Image,
+    );
   });
 });
