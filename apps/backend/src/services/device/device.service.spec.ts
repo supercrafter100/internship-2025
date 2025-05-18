0c4b5246deacbe9238a22b9c8715e19a01a5c693
Ghanoudi Hakim <hakim.ghanoudi@student.ap.be>
2025-05-18 16:48:37 +0300

    Add test for finding a device by id in DeviceService

--- apps/backend/src/services/device/device.service.spec.ts
+++ apps/backend/src/services/device/device.service.spec.ts
@@ -160,4 +160,27 @@ describe('DeviceService', () => {
     expect(result).toEqual(expectedResult);
     expect(mockPrismaService.device.findMany).toHaveBeenCalled();
   });
+
+  it('should find a device by id', async () => {
+    const expectedResult = {
+      id: 'abc123',
+      deviceType: 'sensor',
+      projectId: 1,
+      name: 'Test Device',
+      description: 'Test Description',
+      latitude: '52.3702' as unknown as Decimal,
+      longitude: '4.8952' as unknown as Decimal,
+      imgKey: 'base64ImageString',
+      createdByid: null,
+      createdAt: new Date(),
+    };
+
+    mockPrismaService.device.findUnique.mockResolvedValue(expectedResult);
+
+    const result = await service.findOne('abc123');
+    expect(result).toEqual(expectedResult);
+    expect(mockPrismaService.device.findUnique).toHaveBeenCalledWith({
+      where: { id: 'abc123' },
+    });
+  });
 });
