751697e418720b37a31692df407680ffcaa84375
Ghanoudi Hakim <hakim.ghanoudi@student.ap.be>
2025-05-18 17:00:08 +0300

    Add test for finding all devices for a project in DeviceService

--- apps/backend/src/services/device/device.service.spec.ts
+++ apps/backend/src/services/device/device.service.spec.ts
@@ -249,4 +249,29 @@ describe('DeviceService', () => {
         |> sort(columns:["_time"])`,
     );
   });
+
+  it('should find all devices for a project', async () => {
+    const expectedResult = [
+      {
+        id: 'abc123',
+        deviceType: 'sensor',
+        projectId: 1,
+        name: 'Test Device',
+        description: 'Test Description',
+        latitude: '52.3702' as unknown as Decimal,
+        longitude: '4.8952' as unknown as Decimal,
+        imgKey: 'base64ImageString',
+        createdByid: null,
+        createdAt: new Date(),
+      },
+    ];
+
+    mockPrismaService.device.findMany.mockResolvedValue(expectedResult);
+
+    const result = await service.findAllForProject(1);
+    expect(result).toEqual(expectedResult);
+    expect(mockPrismaService.device.findMany).toHaveBeenCalledWith({
+      where: { projectId: 1 },
+    });
+  });
 });
